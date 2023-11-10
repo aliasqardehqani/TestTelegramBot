9a59ed2b2dcd061be985228f4ad85f4daa73b842
novell <aliasqar.dehqanii@gmail.com>
2023-11-10 21:41:32 +0330

    feat(master): Add two comment for func.

--- telegrambot.py
+++ telegrambot.py
@@ -4,12 +4,12 @@ bot = telebot.TeleBot("Telegram Bot Token")
 
 @bot.message_handler(commands=['start'])
 def send_message(message):
-    bot.send_message(message.chat.id, "Hello KOSIANOooo")
+    bot.send_message(message.chat.id, "Hello KOSIANOooo")  # This is send message when you /start the bot
 
 
 @bot.message_handler(commands=['help'])
 def reply_message(message):
-    bot.reply_to(message, "What do you help?")
+    bot.reply_to(message, "What do you help?")  # When you have need help click /help in BOT
 
 
 bot.infinity_polling()

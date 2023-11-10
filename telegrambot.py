4cccf5c6a8ad2be0db52a73123c6049a66112121
novell <aliasqar.dehqanii@gmail.com>
2023-11-11 01:36:40 +0330

    feat(master): Add callback handler.

--- telegrambot.py
+++ telegrambot.py
@@ -1,33 +1,37 @@
 import telebot
-bot = telebot.TeleBot("")
 
+bot = telebot.TeleBot("6780304179:AAFCtmBJ4y6Z4v_6hjblu_2I0aNUaxyROGc")
 
-# Add inline button to bot for join channel or group or anything
+# Add inline button to bot for joining a channel or group or anything
 button = telebot.types.InlineKeyboardButton("Error Channel", url="https://t.me/+F8mHTtgn-t0yM2Jk")
+callback_button = telebot.types.InlineKeyboardButton("button", callback_data="Hi")
 mark_up = telebot.types.InlineKeyboardMarkup()
-mark_up.add(button)
+mark_up.add(button, callback_button)
 
 # Add Keyboard button to Bot
 keyboard_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
 keyboard_markup.add("parts", "send ticket", 'operator')
 
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    bot.send_message(call.message.chat.id, "You clicked on a button")
+
 @bot.message_handler(commands=['start'])
 def send_message(message):
-    bot.send_message(message.chat.id, "Hello Debugger", reply_markup=mark_up)  # This is send message when you /start the bot
-
+    bot.send_message(message.chat.id, "Hello Debugger", reply_markup=mark_up)
 
 @bot.message_handler(commands=['help'])
 def reply_message(message):
-    bot.reply_to(message, "What do you help?", reply_markup=keyboard_markup)  # When you have need help click /help in BOT
+    bot.reply_to(message, "What do you need help with?", reply_markup=keyboard_markup)
 
 op = "https://t.me/aliasqardehqani"
-@bot.message_handler()
-def keyboard(message):
+@bot.message_handler(func=lambda message: True)
+def handle_messages(message):
     if message.text == "parts":
-        bot.send_message(message.chat.id, "You can use new username")
+        bot.send_message(message.chat.id, "You can use a new username.")
     elif message.text == "send ticket":
-        bot.send_message(message.chat.id, "Please write your ticket : ")
+        bot.send_message(message.chat.id, "Please write your ticket:")
     elif message.text == "operator":
-        bot.send_message(message.chat.id, f"send message to {op}")
+        bot.send_message(message.chat.id, f"Send a message to {op}")
 
 bot.infinity_polling()

c60438b0b8a1b437634af17959f229af2c1f83c8
novell <aliasqar.dehqanii@gmail.com>
2023-11-10 22:13:17 +0330

    feat(master): Keyboard for send help.

--- telegrambot.py
+++ telegrambot.py
@@ -1,11 +1,15 @@
 import telebot
-bot = telebot.TeleBot("6780304179:AAFCtmBJ4y6Z4v_6hjblu_2I0aNUaxyROGc")
+bot = telebot.TeleBot("")
 
 
 # Add inline button to bot for join channel or group or anything
-button = telebot.types.InlineKeyboardButton("CheckThis", url="https://t.me/+F8mHTtgn-t0yM2Jk")
+button = telebot.types.InlineKeyboardButton("Error Channel", url="https://t.me/+F8mHTtgn-t0yM2Jk")
 mark_up = telebot.types.InlineKeyboardMarkup()
 mark_up.add(button)
+
+# Add Keyboard button to Bot
+keyboard_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+keyboard_markup.add("parts", "send ticket", 'operator')
 
 @bot.message_handler(commands=['start'])
 def send_message(message):
@@ -14,7 +18,16 @@ def send_message(message):
 
 @bot.message_handler(commands=['help'])
 def reply_message(message):
-    bot.reply_to(message, "What do you help?")  # When you have need help click /help in BOT
+    bot.reply_to(message, "What do you help?", reply_markup=keyboard_markup)  # When you have need help click /help in BOT
 
+op = "https://t.me/aliasqardehqani"
+@bot.message_handler()
+def keyboard(message):
+    if message.text == "parts":
+        bot.send_message(message.chat.id, "You can use new username")
+    elif message.text == "send ticket":
+        bot.send_message(message.chat.id, "Please write your ticket : ")
+    elif message.text == "operator":
+        bot.send_message(message.chat.id, f"send message to {op}")
 
 bot.infinity_polling()
